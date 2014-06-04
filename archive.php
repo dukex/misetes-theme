a83b78281fc5e64000d1005c7702f09a1b881b2d
Duke <duke@riseup.net>
2014-06-04 16:17:19 -0300

    fixed archive.php

--- archive.php
+++ archive.php
@@ -8,7 +8,6 @@
  */
 
 get_header(); ?>
-archive
 	<section id="primary" class="content-area">
 		<?php if ( have_posts() ) : ?>
 			<header class="page-header">
@@ -21,11 +20,11 @@ archive
 						elseif ( is_author() ) :
 							printf( __( '%s', 'misetes' ), '<span class="vcard">' . get_the_author() . '</span>' );
 						elseif ( is_day() ) :
-							printf( __( 'Day: %s', 'misetes' ), '<span>' . get_the_date() . '</span>' );
+							printf( __( 'Artigos de %s', 'misetes' ), '<span>' . get_the_date() . '</span>' );
 						elseif ( is_month() ) :
-							printf( __( 'Month: %s', 'misetes' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'misetes' ) ) . '</span>' );
+							printf( __( 'Artigos de %s', 'misetes' ), '<span>' . get_the_date( _x( 'F \d\e Y', 'monthly archives date format', 'misetes' ) ) . '</span>' );
 						elseif ( is_year() ) :
-							printf( __( 'Year: %s', 'misetes' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'misetes' ) ) . '</span>' );
+							printf( __( 'Artigos de %s', 'misetes' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'misetes' ) ) . '</span>' );
 						elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
 							_e( 'Asides', 'misetes' );
 						elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) :
